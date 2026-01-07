9dac74d4f32bc4f1c4ded7a30297e40083ad606b
YasharPa <yasharpashaiy1@gmail.com>
2026-01-07 23:52:54 +0200

    feat: complete vocabulary api implementation

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const vocabularyRoutes = require("./routes/vocabularyRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -12,7 +12,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server is running perfectly! ");
 });
-
+app.use("/api/vocabulary", vocabularyRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
