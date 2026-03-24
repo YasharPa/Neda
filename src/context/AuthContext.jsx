45a4a90ea1093f27a823ec53826defc1c1300d44
YasharPa <yasharpashaiy1@gmail.com>
2026-03-24 15:40:02 +0200

    feat: add AuthContext

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,52 @@
+import { createContext, useEffect, useState } from "react";
+import { supabase } from "../lib/supabaseClient";
+
+const AuthContext = createContext({});
+
+export function AuthProvider({ children }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const getInitialSession = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+      setUser(session?.user ?? null);
+      setIsLoading(false);
+    };
+
+    getInitialSession();
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user ?? null);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  const signIn = async (email, password) => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+    return { data, error };
+  };
+
+  const signUp = async (email, password, metadata) => {
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+      options: { data: metadata },
+    });
+
+    return { data, error };
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, isLoading, signIn, signUp }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
